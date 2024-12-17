7f1b768f47061c8a9ebbdf855107b74de1f9f03a
Saim-Ahmed007 <saimahmed.seu.cse@gmail.com>
2024-12-17 12:32:39 +0600

    create doctor appointment home page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { assets } from "../assets/assets_frontend/assets"
+
+
+const Footer = () => {
+  return (
+    <div className="md:mx-10">
+
+        <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+            <div>
+                <img className="mb-5 w-40" src={assets.logo} alt="" />
+                <p className="w-full md:w-2/3 text-gray-600 leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus inventore quos quae dolore maiores enim laboriosam dolorum voluptatum ea. Adipisci.</p>
+            </div>
+            <div>
+                <p className="text-xl font-medium mb-5">COMPANY</p>
+                <ul className="flex flex-col gap-2 text-gray-600">
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Privacy Policy</li>
+                </ul>
+
+            </div>
+            <div>
+                <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+                <ul className="flex flex-col gap-2">
+                    <li className="text-gray-600">01701351383</li>
+                    <li className="text-gray-600">saimahmed.seu.cse@gmail.com</li>
+                </ul>
+
+            </div>
+
+        </div>
+
+        <div>
+            <hr />
+            <p className="py-5 text-sm text-center">Copyright 2024@ Saim Ahmed - All Rights Reserved</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Banner from "../components/Banner"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import SpecialityMenu from "../components/SpecialityMenu"
+import TopDoctors from "../components/TopDoctors"
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <SpecialityMenu/>
+      <TopDoctors/>
+      <Banner/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Home
